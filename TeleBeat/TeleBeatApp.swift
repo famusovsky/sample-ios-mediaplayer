50ef13d91cfff1b76442d75bb7e4df6c064feb43
Alexey Stepanov <st3p-by-step@ya.ru>
2023-02-12 18:22:56 +0300

    Initial Commit

---
+++ TeleBeat/TeleBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeleBeatApp.swift
+//  TeleBeat
+//
+//  Created by Алексей Степанов on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct TeleBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
